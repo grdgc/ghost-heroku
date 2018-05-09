45102264e526f07a95ecfcdbfcfcfbe3402d51e4
Seth Bergman <seth.atxwebs@gmail.com>
2018-05-09 05:52:35 -0500

    deploy

--- bin/common/env-values.js
+++ bin/common/env-values.js
@@ -1,3 +1,8 @@
 module.exports = {
-  mysqlDatabaseUrl: process.env.MYSQL_DATABASE_URL || process.env.JAWSDB_URL || process.env.CLEARDB_DATABASE_URL
+  mysqlDatabaseUrl:
+    process.env.MYSQL_DATABASE_URL ||
+    process.env.JAWSDB_URL ||
+    process.env.CLEARDB_DATABASE_URL ||
+    process.env.BUCKETEER_AWS_ACCESS_KEY_ID ||
+    process.env.MAILGUN_API_KEY,
 }
